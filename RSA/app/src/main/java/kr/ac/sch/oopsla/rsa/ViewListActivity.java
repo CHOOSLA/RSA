32cf6f3ee1868e5d637d2328c3fdde139e9d2dc7
Choo Chang Woo <cnckddn0146@gmail.com>
2022-09-21 22:05:42 +0900

    View & Send 수정 및 테마를 기존의 테마로 변경 작업

---
+++ RSA/app/src/main/java/kr/ac/sch/oopsla/rsa/ViewListActivity.java
@@ -0,0 +1,263 @@
+package kr.ac.sch.oopsla.rsa;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import kr.ac.sch.oopsla.rsa.db.DBtype;
+
+public class ViewListActivity extends Activity implements View.OnClickListener {
+    private Button btnCallNext;
+    private Button btnCallBack;
+    private TextView mDateDisplay1;
+    private TextView mDateDisplay2;
+    private String endDay;
+    private String startDay;
+    private CustomAdapter mCustomAdapter = null;
+    private ArrayList<String> mArrayList = new ArrayList<String>();
+    private Set<String> mArrayDateList = new HashSet<String>();
+
+    private ArrayList<String> mCheckArrayList = new ArrayList<String>();
+
+    private ListView mListView = null;
+    private CheckBox mAllCheckBox = null;
+    private DBtype db = null;
+    Cursor cursor = null;
+    private int position = 0;
+    private String Selected;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        Intent intent = getIntent();
+        endDay = intent.getStringExtra("endDay");
+        startDay = intent.getStringExtra("startDay");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_list);
+        setLayout();
+        MainMethod();
+
+
+        btnCallBack = (Button) findViewById(R.id.button_result_list_back);
+        btnCallBack.setOnClickListener(this);
+        btnCallNext = (Button) findViewById(R.id.button_result_list_next);
+        btnCallNext.setOnClickListener(this);
+
+        mDateDisplay1 = (TextView)findViewById(R.id.text_result_list_start);
+        String[] arrayDay = startDay.split("-");
+        mDateDisplay1.setText(arrayDay[1]+"-"+arrayDay[2]+"-"+arrayDay[0]);
+
+        mDateDisplay2 = (TextView)findViewById(R.id.text_result_list_end);
+
+        arrayDay = endDay.split("-");
+        mDateDisplay2.setText(arrayDay[1]+"-"+arrayDay[2]+"-"+arrayDay[0]);
+
+//		mDateDisplay2.setText(Selected+"");
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if (id == R.id.button_result_list_back) { // back
+            finish();
+        }
+        else if (id == R.id.button_result_list_next) { // next
+            int count = mCustomAdapter.getChecked().size();
+
+
+                if(count == 0) {
+                    Toast.makeText(getApplicationContext(), "Select at least one!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    for(int i = 0; i < mCustomAdapter.getChecked().size(); i++) {
+                        position = mCustomAdapter.getChecked().get(i);
+                        mCheckArrayList.add(mArrayList.get(position));
+                    }
+
+
+                    Intent intent1 = new Intent(ViewListActivity.this,ViewResultActivity.class);
+                    intent1.putExtra("Date", mCheckArrayList);
+                    startActivity(intent1);
+
+                }
+
+        }
+    }
+
+    // Custom Adapter
+    class CustomAdapter extends BaseAdapter {
+
+        private ViewHolder viewHolder = null;
+        // ?뜝?뜲????? ?뜝?룞?삕?뜝?룞?삕 ?뜝?룞?삕?뜝?룞?삕?뜝????? ?뜝?룞?삕?뜝?룞?삕 Inflater
+        private LayoutInflater inflater = null;
+        private ArrayList<String> sArrayList = new ArrayList<String>();
+        private boolean[] isCheckedConfrim;
+
+        public CustomAdapter (Context c , ArrayList<String> mList) {
+            inflater = LayoutInflater.from(c);
+            this.sArrayList = mList;
+            // ArrayList Size ?뜝?룞?삕?겮?뜝?룞?삕 boolean ?뜝?띁?뿴?뜝?룞?삕 ?뜝?룞?삕?뜝?룞?삕?뜝?????.
+            // CheckBox?뜝?룞?삕 true/false?뜝?룞?삕 ?뜝?룞?삕?뜝?룞?삕 ?뜝?떦源띿??? ?뜝?룞?삕?뜝?룞?삕
+            this.isCheckedConfrim = new boolean[sArrayList.size()];
+        }
+
+        // CheckBox?뜝?룞?삕 ?뜝?룞?삕?뜝????? ?뜝?룞?삕?뜝?룞?삕?뜝?떦?뙋?삕 ?뜝?뙣?눦?삕?뜝?룞?삕
+        public void setAllChecked(boolean ischeked) {
+            int tempSize = isCheckedConfrim.length;
+            for(int a=0 ; a<tempSize ; a++){
+                isCheckedConfrim[a] = ischeked;
+            }
+        }
+
+        public void setChecked(int position) {
+            isCheckedConfrim[position] = !isCheckedConfrim[position];
+        }
+
+        public ArrayList<Integer> getChecked(){
+            int tempSize = isCheckedConfrim.length;
+            ArrayList<Integer> mArrayList = new ArrayList<Integer>();
+            for(int b=0 ; b<tempSize ; b++){
+                if(isCheckedConfrim[b]){
+                    mArrayList.add(b);
+                }
+            }
+            return mArrayList;
+        }
+
+        @Override
+        public int getCount() {
+            return sArrayList.size();
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            // ConvertView?뜝?룞?삕 null ?뜝?룞?삕 ?뜝?룞?삕?뜝?????
+            View v = convertView;
+
+            if( v == null ){
+                viewHolder = new ViewHolder();
+                // View?뜝?룞?삕 inflater ?뜝?룞?삕?뜝?룞?삕?뜝?뙏?뙋?삕.
+                v = inflater.inflate(R.layout.item_view_list, null);
+                viewHolder.cBox = (CheckBox) v.findViewById(R.id.checkBoxListPeriod);
+                v.setTag(viewHolder);
+            }
+
+            else {
+                viewHolder = (ViewHolder)v.getTag();
+            }
+
+            // CheckBox?뜝?룞?삕 ?뜝?뜦蹂멨????룞?삕?뜝?룞?삕?뜝?룞?삕 ?뜝?떛??????????듃?뜝?룞?삕 ?뜝?룞?삕?뜝?룞?삕?뜝?룞?삕 ?뜝?뙇源띿??? ?뜝?룞?삕?뜝?룞?삕?뜝?룞?삕 ListView?뜝?룞?삕 ?뜝?룞?삕?뜝?룞?삕?뜝?룞?삕
+            // ?겢?뜝?룞?삕?뜝?룞?삕?뜝?룞?삕鼇뜹???????? ?뜝?룞?삕?뜝?룞?삕閭먨???????? ?뜝?룞?삕?뜝?뙏?눦?삕?뜝?룞?삕 CheckBox?뜝?룞?삕 ?뜝?떛??????????듃?뜝?룞?삕 ?뜝?룞?삕?뜝?룞?삕 ?뜝?뙇?뼲?삕?뜝????? ?뜝?떬?뙋?삕.
+            viewHolder.cBox.setClickable(false);
+            viewHolder.cBox.setFocusable(false);
+
+            viewHolder.cBox.setText(sArrayList.get(position));
+            // isCheckedConfrim ?뜝?띁?뿴?뜝?룞?삕 ?뜝?떗源띿????솕?뜝?룞?삕 ?뜝?룞?삕?뜝????? false?뜝?룞?삕 ?뜝?떗源띿????솕 ?뜝?떎湲곕븣?????룞?삕?뜝?룞?삕
+            // ?뜝?뜦蹂멨????룞?삕?뜝?룞?삕?뜝?룞?삕 false?뜝?룞?삕 ?뜝?떗源띿????솕 ?뜝?룞?삕?궗 ?뜝?룞?삕 ?뜝?뙇?뙋?삕.
+            viewHolder.cBox.setChecked(isCheckedConfrim[position]);
+
+            return v;
+        }
+    }
+
+    class ViewHolder {
+        // ?뜝?룞?삕?뜝?떥?슱?삕 Row?뜝?룞?삕 ?뜝?룞?삕????????? CheckBox
+        private CheckBox cBox = null;
+    }
+
+    private void setLayout(){
+        mListView = (ListView) findViewById(R.id.listview_result_list_list);
+
+        mAllCheckBox = (CheckBox) findViewById(R.id.checkbox_result_list_checkbox);
+        // ?뜝?룞?삕泥? 泥댄?? ?뜝?룞?삕?듉 ?겢?뜝?룞?삕?뜝?룞?삕 Listener
+        mAllCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCustomAdapter.setAllChecked(mAllCheckBox.isChecked());
+                // Adapter?뜝?룞?삕 Data?뜝?룞?삕 ?뜝?룞?삕?솕?뜝?룞?삕 ?뜝?룞?삕?뜝?룞?삕?뜝?룞?삕?뜝?룞?삕 Adapter?뜝?룞?삕 ?뜝?떙琉꾩????뜝?뙏?뙋?삕.
+                mCustomAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+
+    private void MainMethod() {
+        String query = null;
+
+            query = "SELECT * FROM DATA2";
+
+
+//		Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_SHORT).show();
+//
+//		query = "SELECT * FROM DATA2";
+
+        mArrayList = new ArrayList<String>();
+
+        db = new DBtype(ViewListActivity.this);
+        cursor = db.selectRawQuery(query);
+
+        String s, c;
+
+        if(cursor.moveToFirst()){
+            do{
+                s = cursor.getString(cursor.getColumnIndex("date"));
+                c = cursor.getString(cursor.getColumnIndex("time"));
+
+                if(s==null || c==null){
+                    break;
+                }
+                Log.e(s, c);
+                if(startDay.compareTo(s) <= 0 && endDay.compareTo(s)>= 0){
+                    mArrayList.add(s + " " + c);
+                }
+            }while(cursor.moveToNext());
+        }
+
+        //mArrayList = new ArrayList<String>(mArrayDateList);
+
+        mCustomAdapter = new CustomAdapter(ViewListActivity.this , mArrayList);
+        mListView.setAdapter(mCustomAdapter);
+        mListView.setOnItemClickListener(mItemClickListener);
+    }
+
+
+    private AdapterView.OnItemClickListener mItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+            mCustomAdapter.setChecked(position);
+            // Data ?뜝?룞?삕?뜝?룞?삕?뜝????? ?샇?뜝?룞?삕 Adapter?뜝?룞?삕 Data ?뜝?룞?삕?뜝?룞?삕 ?뜝?룞?삕?뜝?룞?삕?뜝????? ?뜝?떙琉꾩????뜝?뛿?꽌 Update ?뜝?룞?삕.
+            mCustomAdapter.notifyDataSetChanged();
+
+        }
+    };
+}
---
+++ RSA/app/src/main/java/kr/ac/sch/oopsla/rsa/ViewSelectActivity.java
@@ -0,0 +1,225 @@
+package kr.ac.sch.oopsla.rsa;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import kr.ac.sch.oopsla.rsa.androidqa.ArrayWheelAdapter;
+import kr.ac.sch.oopsla.rsa.androidqa.OnWheelChangedListener;
+import kr.ac.sch.oopsla.rsa.androidqa.OnWheelScrollListener;
+import kr.ac.sch.oopsla.rsa.androidqa.WheelView;
+import kr.ac.sch.oopsla.rsa.process.calendarProcess;
+import ru.slybeaver.slycalendarview.SlyCalendarDialog;
+
+public class ViewSelectActivity extends AppCompatActivity implements SlyCalendarDialog.Callback {
+
+    Context ctx;
+    boolean wheelScrolled ;
+    String wheelMenu1[] = new String[]{"1","2","3","4","5","6","7","8","9","10"};
+    String wheelMenu2[] = new String[]{"Days", "Weeks", "Months"};
+    ImageButton mImgBtnCalendar;
+
+    private String today;
+    private String changeDay;
+    private WheelView wvNum;
+    private WheelView wvWord;
+
+    Button mBtnBack,mBtnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_select);
+
+        // wheel 부분 설정
+        ctx = ViewSelectActivity.this;
+        initWheelDay(R.id.lmcp2);
+        initWheelNum(R.id.lmcp1);
+
+        wvNum = (WheelView)findViewById(R.id.lmcp1);
+        wvWord = (WheelView)findViewById(R.id.lmcp2);
+
+        mImgBtnCalendar = (ImageButton) findViewById(R.id.ImgButton_Calendar);
+
+        // SlyCalendar 설정
+
+        FragmentManager fgManager = getSupportFragmentManager();
+        mImgBtnCalendar.setOnClickListener((v) -> {
+            new SlyCalendarDialog()
+                    .setSingle(false)
+                    .setCallback(this)
+                    .show(fgManager, "TAG_SLYCALENDAR");
+        });
+
+
+        // SlyCalendar가 종료되었을 때 뜨는 콜백 메소드
+        fgManager.registerFragmentLifecycleCallbacks(new FragmentManager.FragmentLifecycleCallbacks() {
+            @Override
+            public void onFragmentDestroyed(@NonNull FragmentManager fm, @NonNull Fragment f) {
+                super.onFragmentDestroyed(fm, f);
+                Log.e("SlyCalendar","SlyCalendar Destroyed");
+                nextPage(changeDay,today);
+            }
+        }, true);
+
+        today = new SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault()).format(new Date().getTime());
+
+        mBtnBack = findViewById(R.id.button_result_view_back);
+        mBtnNext = findViewById(R.id.button_result_view_next);
+
+        mBtnBack.setOnClickListener((v) -> { finish();});
+        mBtnNext.setOnClickListener((v) -> {nextPage(changeDay,today);});
+
+    }
+
+
+
+    // wheel 부분
+
+    private void initWheelDay(int id)
+    {
+        WheelView wheel = (WheelView) findViewById(id);
+        wheel.setViewAdapter(new ArrayWheelAdapter(ctx,wheelMenu2));
+        wheel.setVisibleItems(2);
+        wheel.setCurrentItem(0);
+        wheel.addChangingListener(changedListener);
+        wheel.addScrollingListener(scrolledListener);
+    }
+
+    private void initWheelNum(int id)
+    {
+        WheelView wheel = (WheelView) findViewById(id);
+        wheel.setViewAdapter(new ArrayWheelAdapter(ctx, wheelMenu1));
+        wheel.setVisibleItems(2);
+        wheel.setCurrentItem(0);
+        wheel.addChangingListener(changedListener);
+        wheel.addScrollingListener(scrolledListener);
+    }
+
+    OnWheelScrollListener scrolledListener = new OnWheelScrollListener()
+    {
+        public void onScrollStarts(WheelView wheel)
+        {
+            wheelScrolled = true;
+        }
+
+        public void onScrollEnds(WheelView wheel)
+        {
+            wheelScrolled = false;
+        }
+
+        @Override
+        public void onScrollingStarted(WheelView wheel) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onScrollingFinished(WheelView wheel) {
+            // TODO Auto-generated method stub
+
+        }
+    };
+
+    // Wheel changed listener
+    private final OnWheelChangedListener changedListener = new OnWheelChangedListener()
+    {
+        public void onChanged(WheelView wheel, int oldValue, int newValue)
+        {
+            if (!wheelScrolled)
+            {
+                updateStatus();
+            }
+        }
+    };
+
+    /**
+     * Updates entered PIN status
+     */
+    private void updateStatus()
+    {
+
+        changeDay = calendarProcess.subDay(today, wvNum.getCurrentItem()+1, wvWord.getCurrentItem());
+        Toast.makeText(this,changeDay,Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    // 여기서 부턴 SlyCalendar 부분
+
+    @Override
+    public void onDataSelected(Calendar firstDate, Calendar secondDate, int hours, int minutes) {
+
+        String firstDateFormat = new SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault()).format(firstDate.getTime());
+        String secondDateFormat = new SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault()).format(secondDate.getTime());
+
+        if (firstDate != null) {
+
+            // 하나의 날짜만 선택됬을 경우
+            if (secondDate == null) {
+                firstDate.set(Calendar.HOUR_OF_DAY, hours);
+                firstDate.set(Calendar.MINUTE, minutes);
+                /*
+                Toast.makeText(
+                        this,
+                        new SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault()).format(firstDate.getTime()),
+                        Toast.LENGTH_LONG
+
+                ).show();
+
+                 */
+
+                changeDay = firstDateFormat;
+                today = firstDateFormat;
+
+            } else {
+                // 기간이 선택이 되었을 경우
+                /*
+                Toast.makeText(
+                        this,
+                        getString(
+                                R.string.period,
+                                new SimpleDateFormat(getString(R.string.dateFormat), Locale.getDefault()).format(firstDate.getTime()),
+                                new SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault()).format(secondDate.getTime())
+                        ),
+                        Toast.LENGTH_LONG
+
+                ).show();
+
+                 */
+
+                changeDay = firstDateFormat;
+                today = secondDateFormat;
+
+            }
+        }
+    }
+
+    @Override
+    public void onCancelled() {
+
+    }
+
+    private void nextPage(String changeDay,String today){
+        Intent intent1 = new Intent(ViewSelectActivity.this,ViewListActivity.class);
+        intent1.putExtra("startDay", changeDay);
+        intent1.putExtra("endDay", today);
+        startActivity(intent1);
+    }
+}
+
